06d4aad21cc95f8f27af3da5fb938eac1b81543b
Tanmay Mishra <tanmaycloud251@gmail.com>
2026-02-03 23:55:53 +0530

    Add Button, Input, PostCard, and Select components with improved structure and styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 function Button({
     children,
     type = "button",
-    bgColor = "bg-blue-600"
+    bgColor = "bg-blue-600",
+    textColor = "white",
+    className = '',
+    ...props
 }) {
   return (
-    <button className='inline-block'>{children}</button>
+    <button className={`px-4 py-2 rounded-lg ${className} ${bgColor} ${textColor}`} {...props}>{children}</button>
   )
 }
 
