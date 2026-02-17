e7bcc6dd82e14e19029bd9be49fef188cedac24f
Tanmay Mishra <tanmaycloud251@gmail.com>
2026-02-18 01:09:19 +0530

    Add PostForm component with form handling and RTE integration

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer"
 import Container from "./Container"
 import LogoutBtn from "./LogoutBtn"
 import Logo from "./Logo"
+import RTE from "./RTE"
 
-export {Header, Footer, Container, LogoutBtn, Logo}
+export {Header, Footer, Container, LogoutBtn, Logo, RTE}
